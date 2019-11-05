8784a9fedd930e36b2ec124e974de585daa03f8e
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-11-05 08:36:54 +0100

    SDL2 Init.

--- c/src/main.c
+++ c/src/main.c
@@ -1,5 +1,23 @@
 #include <SDL2/SDL.h>
 
 int main(void) {
+
+    SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "An SDL2 window",                  // window title
+        SDL_WINDOWPOS_UNDEFINED,           // initial x position
+        SDL_WINDOWPOS_UNDEFINED,           // initial y position
+        640,                               // width, in pixels
+        480,                               // height, in pixels
+        SDL_WINDOW_OPENGL                  // flags - see below
+    );
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
