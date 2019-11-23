0555d9f933d1bf8388a48bcfc062b1ba6245917d
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-11-23 16:56:41 +0100

    Enemies collision (poor).

--- python/lib/enemy.py
+++ python/lib/enemy.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 
 from lib.constants import ENEMY_SPEED, COLORS
@@ -15,9 +16,43 @@ class Enemy:
 
         # Movement
         self.speed = ENEMY_SPEED
+        self.dirH = 0
+        self.dirW = 0
 
-        # Rect
+        # Rect (camera position)
         self.rect = (self.posW, self.posH, self.rectW, self.rectH)
+
+    def collides(self, enemy):
+        condH = (
+            self.posH < enemy.posH + enemy.rectH
+            and self.posH > enemy.posH - self.rectH
+        )
+        condW = (
+            self.posW < enemy.posW + enemy.rectW
+            and self.posW > enemy.posW - self.rectW
+        )
+        return condH and condW
+
+    def reset_contact(self, enemy, dt):
+        """
+        Reset position to contact position, before collision.
+        """
+        # Collision direction
+        h = self.dirH - enemy.dirH
+        w = self.dirW - enemy.dirW
+        length = math.sqrt(h ** 2 + w ** 2)
+        collision_dirH = h / length
+        collision_dirW = w / length
+
+        # Overlap gap
+        gapH = collision_dirH * (self.rectH - abs(self.posH - enemy.posH))
+        gapW = collision_dirW * (self.rectW - abs(self.posW - enemy.posW))
+
+        # Reset
+        if gapH > gapW:
+            self.posH -= gapH
+        else:
+            self.posW -= gapW
 
     def event(self):
         """
@@ -25,26 +60,36 @@ class Enemy:
         """
         pass
 
-    def update(self, character, camera, dt):
+    def update(self, character, enemies, camera, dt):
         """
         AI for movement.
         """
+        # Set direction and normalize
         h = character.posH - self.posH
-        directionH = h / abs(h)
         w = character.posW - self.posW
-        directionW = w / abs(w)
+        length = math.sqrt(h ** 2 + w ** 2)
+        self.dirH = h / length
+        self.dirW = w / length
 
-        self.posW += directionW * self.speed * dt
-        self.posH += directionH * self.speed * dt
-        self.rect = (self.posW - camera.posW,
-                     self.posH - camera.posH,
-                     self.rectW,
-                     self.rectH)
+        # Update position
+        self.posW += self.dirW * self.speed * dt
+        self.posH += self.dirH * self.speed * dt
+
+        # Check collision with other enemies
+        for enemy in enemies:
+            if enemy != self and self.collides(enemy):
+                self.reset_contact(enemy, dt)
+
+        # Camera position
+        self.rect = (
+            self.posW - camera.posW,
+            self.posH - camera.posH,
+            self.rectW,
+            self.rectH,
+        )
 
     def draw(self, camera):
         """
         Display rect.
         """
-        pygame.draw.rect(camera.screen,
-                         COLORS['GREEN'],
-                         self.rect)
+        pygame.draw.rect(camera.screen, COLORS["GREEN"], self.rect)
