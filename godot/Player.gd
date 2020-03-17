1554f7bf5a6888ae1077db41342d21ce5e60b4c2
Geoffrey Jount <geoffrey.jount@epita.fr>
2020-03-17 10:05:32 +0100

    Clean Player script.

--- godot/Player.gd
+++ godot/Player.gd
@@ -3,18 +3,16 @@ extends Area2D
 export var speed = 300
 var screen_size
 var look
-var attacking
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
     screen_size = get_viewport_rect().size
     look = Vector2()
-    attacking = false
     $AnimatedSprite.play()
-    
 
 
 func _process(delta):
+    # Movement
     var velocity = Vector2()
     if Input.is_action_pressed("ui_right"):
         velocity.x += 1
@@ -24,7 +22,12 @@ func _process(delta):
         velocity.y += 1
     if Input.is_action_pressed("ui_up"):
         velocity.y -= 1
-        
+    velocity = velocity.normalized() * speed
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screen_size.x - 50)
+    position.y = clamp(position.y, 0, screen_size.y - 50)
+
+    # Look
     if Input.is_action_pressed("look_right"):
         look.x += 1
     if Input.is_action_pressed("look_left"):
@@ -33,23 +36,12 @@ func _process(delta):
         look.y += 1
     if Input.is_action_pressed("look_up"):
         look.y -= 1
-
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed
-        if not attacking:
-            $AnimatedSprite.animation = "run"
-    else:
-        if not attacking:
-            $AnimatedSprite.animation = "idle"
-
-    position += velocity * delta
-    position.x = clamp(position.x, 0, screen_size.x)
-    position.y = clamp(position.y, 0, screen_size.y)
-    
-    if Input.is_action_pressed("attack"):
-        attacking = true
-    if attacking:
-        $AnimatedSprite.animation = "attack"
-        
     look = look.normalized()
+
+    # Animation
+    if velocity.length() > 0:
+        $AnimatedSprite.animation = "run"
+    else:
+        $AnimatedSprite.animation = "idle"
+
     $AnimatedSprite.flip_h = look.x < 0
